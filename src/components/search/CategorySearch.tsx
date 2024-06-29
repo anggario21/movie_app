3df15790f2818ddbe2e840c70f76117e4f8bb423
anggario21d <anggario_21@apps.ipb.ac.id>
2024-06-29 20:32:58 +0700

    add search

---
+++ src/components/search/CategorySearch.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class CategorySearch extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CategorySearch</Text>
+      </View>
+    );
+  }
+}
+
+export default CategorySearch;
---
+++ src/components/search/KeywordSearch.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class KeywordSearch extends Component {
+  render() {
+    return (
+      <View>
+        <Text>KeywordSearch</Text>
+      </View>
+    );
+  }
+}
+
+export default KeywordSearch;
